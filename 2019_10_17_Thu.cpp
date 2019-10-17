da7fe7c1815a8b66e7eeb8ba74d19e58bfa48c26
Yujin Chung <yujin.mar.chung@gmail.com>
2019-10-17 20:43:04 +0900

    Learned_how2compileC++andRun

---
+++ 2019_10_17_Thu.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	cout <<"Hey Yujin"; 
+	return 0;
+
+
+/* 
+file name: cpp_study.cpp
+terminal C++ install check: g++ help
+compile C++ file: g++ cpp_study.cpp -o a2 
+run the copiled file: ./a2
+
+*/
