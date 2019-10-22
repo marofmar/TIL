1ae04af600515355bf7a67d1a60e7b41e1fb7cc4
Yujin Chung <yujin.mar.chung@gmail.com>
2019-10-22 13:03:35 +0900

    TIL_go first study

---
+++ 2019_10_22_Tue.go
@@ -0,0 +1,19 @@
+/*
+main.go
+
+
+<compile!>
+go build main.go
+./main 
+
+<run> - this does not make an executable, binary file, unlike 'go build'
+go run main.go
+
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
