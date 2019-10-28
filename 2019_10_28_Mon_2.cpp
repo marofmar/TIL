2967fa6928f72154f4209e22fdd815997979883f
Yujin Chung <yujin.mar.chung@gmail.com>
2019-10-28 19:03:49 +0900

    Update 2019_10_28_Mon_2.cpp

--- 2019_10_28_Mon_2.cpp
+++ 2019_10_28_Mon_2.cpp
@@ -6,7 +6,16 @@ int main(int argc, const char * argv[]) {
     std::string TorF[] = {"False","True"};
     int a = 100;
     int b = 33;
+    int A = 3;
+    int B = 6;
+    int C = 9;
+    int D = 12;
     
     std::cout<<"a < b is "<<TorF[a<b]<<std::endl;
+    
+    std::cout<<"(A ==C) && (B > D) is "<<TorF[(A ==C) && (B > D)];
+
+    //A true || false = true
+    std::cout<<"\n(A ==C) || (B == D) is "<<TorF[(A ==C) || (B == D)]<<std::endl;
     return 0;
 }
