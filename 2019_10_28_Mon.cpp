4d517e6d4cc43dd576362c703a491c63919f613f
Yujin Chung <yujin.mar.chung@gmail.com>
2019-10-28 15:57:18 +0900

    Cpp_first hello world

---
+++ 2019_10_28_Mon.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Yujin Chung on 28/10/2019.
+//  Copyright © 2019 Yujin Chung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! This is my first (not true) cpp program. "<<std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+/*
+X code
+MacOS
+command line
+
+build and run!
+
+
+*/
