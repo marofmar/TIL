bb7b0af0ed71e7c43f11eb1cf6547fe899c39a6b
Yujin Chung <yujin.mar.chung@gmail.com>
2019-11-12 23:58:45 +0900

    cpp_startClassChpaterStudy

---
+++ 2019_11_12_Tue.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  classCounter
+//
+//  Created by Yujin Chung on 12/11/2019.
+//  Copyright © 2019 Yujin Chung. All rights reserved.
+//
+
+#ifndef COUNTER_H_INCLUDED
+#define COUNTER_H_INCLUDED
+
+class Counter {
+    int value;
+public:
+    void resset()
+        { value = 0;}
+    
+}
