726d5e3e4aa928ca0adde1d6b63935de184f695b
Yujin Chung <yujin.mar.chung@gmail.com>
2019-11-29 19:01:12 +0900

    Create 2019_11_29_Fri.py

---
+++ 2019_11_29_Fri.py
@@ -0,0 +1,8 @@
+'''
+Leetcode 
+figuring out the existence of duplicates
+'''
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
