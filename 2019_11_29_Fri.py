86dcbfdb094456d066d5fb55936f4a9cb6324379
Yujin Chung <yujin.mar.chung@gmail.com>
2019-11-29 21:43:58 +0900

    Update 2019_11_29_Fri.py

--- 2019_11_29_Fri.py
+++ 2019_11_29_Fri.py
@@ -6,3 +6,21 @@ figuring out the existence of duplicates
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
+
+    
+
+'''
+LeetCode 
+Return value that only appears once. 
+'''
+class Solution: # find a value that only comes once. Others come twice. 
+    def singleNumber(self, nums: List[int]) -> int:
+        tmp = set({}) # empty set
+        for num in nums:
+            if num in tmp:
+                tmp.remove(num)
+            else:
+                tmp.add(num)
+                
+        return list(tmp)[0]
+        
